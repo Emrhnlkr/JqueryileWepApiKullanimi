f2c629d8746b0498e0bd27d3fa51ced1b410a88d
Emrhnlkr <39797013+Emrhnlkr@users.noreply.github.com>
2018-10-24 14:42:41 +0300

    Jquery

---
+++ JqueryKullanımı/JqueryKullanımı/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqueryKullanımı.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ JqueryKullanımı/JqueryKullanımı/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JqueryKullanımı.Global" Language="C#" %>
